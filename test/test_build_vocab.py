c288310f57785173f578c0698413c0cda8584d0b
nulls8 <moon-hotel@hotmail.com>
2023-07-31 21:25:09 +0800

    添加词表构建模块

---
+++ test/test_build_vocab.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+sys.path.append('../')
+
+from utils.data_helper import Vocab
+from utils.data_helper import my_tokenizer
+from utils import DATA_HOME
+from utils import logger_init
+from collections import Counter
+import logging
+
+if __name__ == '__main__':
+    logger_init(log_file_name='log', log_level=logging.INFO, log_dir='./log')
+    path_de = os.path.join(DATA_HOME, 'GermanEnglish', 'train_.de')
+    tokenizer = my_tokenizer()
+    vocab = Vocab(tokenizer['src'], file_path=path_de, min_freq=2, top_k=None)
+    logging.info(vocab.stoi)
+    logging.info(vocab.itos)
+    logging.info(len(vocab))
+
