3583097f918f87122c010619d9a0bce0942faff9
nulls8 <moon-hotel@hotmail.com>
2023-08-01 21:08:27 +0800

    优化代码，增加词表缓存、预处理缓存

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,8 @@
 import os
 from .log_manage import logger_init
 from .data_helper import LoadEnglishGermanDataset
-
-PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_HOME = os.path.join(PROJECT_HOME, 'data')
+from .data_helper import PROJECT_HOME
+from .data_helper import DATA_HOME
 
 __all__ = [
     'PROJECT_HOME',
--- utils/data_helper.py
+++ utils/data_helper.py
@@ -1,15 +1,21 @@
-import logging
 from collections import Counter
 import torch
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 from torchtext.data.utils import get_tokenizer
 from tqdm import tqdm
+import logging
+import os
+from .tools import process_cache
+from copy import deepcopy
+
+PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_HOME = os.path.join(PROJECT_HOME, 'data')
 
 
 def my_tokenizer():
     """
     分词器
+    本示例中使用到的tokenizer需要安装spacy包和下面这两个包，data目录下有提供
     pip install de_core_news_sm-3.0.0.tar.gz
     pip install en_core_web_sm-3.0.0.tar.gz
     :return:
@@ -19,10 +25,45 @@ def my_tokenizer():
     """
     tokenizer = {}
     # 源序列分词器
-    tokenizer['src'] = get_tokenizer('spacy', language='de_core_news_sm')  # 德语
+
+    # 这里如果是换成别的平行语料，则需要在这里实现对应的分词器
+    tokenizer['src'] = get_tokenizer('spacy', language='de_core_news_sm')  # 源序列（这里是德语）
     # 目标序列分词器
-    tokenizer['tgt'] = get_tokenizer('spacy', language='en_core_web_sm')  # 英语
+    tokenizer['tgt'] = get_tokenizer('spacy', language='en_core_web_sm')  # 目标序列（这里是英语）
     return tokenizer
+
+
+def pad_sequence(sequences, batch_first=False, max_len=None, padding_value=0):
+    """
+    对一个List中的元素进行padding
+    Pad a list of variable length Tensors with ``padding_value``
+    a = torch.ones(25)
+    b = torch.ones(22)
+    c = torch.ones(15)
+    pad_sequence([a, b, c],max_len=None).size()
+    torch.Size([25, 3])
+
+    sequence: 为list, 每个元素为一个tensor
+    batch_first: 是否把batch_size放到第一个维度
+    padding_value: padding值
+    max_len :
+            当max_len = 50时，表示以某个固定长度对样本进行padding，如有多余则截掉；
+            当max_len=None是，表示以当前batch中最长样本的长度对其它进行padding。
+    """
+    if max_len is None:
+        max_len = max([s.size(0) for s in sequences])
+    out_tensors = []
+    for tensor in sequences:
+        if tensor.size(0) < max_len:
+            padding_content = [padding_value] * (max_len - tensor.size(0))
+            tensor = torch.cat([tensor, torch.tensor(padding_content)], dim=0)
+        else:
+            tensor = tensor[:max_len]
+        out_tensors.append(tensor)
+    out_tensors = torch.stack(out_tensors, dim=1)
+    if batch_first:
+        return out_tensors.transpose(0, 1)
+    return out_tensors
 
 
 class Vocab(object):
@@ -59,13 +100,19 @@ class Vocab(object):
         self.file_path = file_path
         self.min_freq = min_freq
         self.top_k = top_k
-        self.stoi = {specials[0]: 0, specials[1]: 1, specials[2]: 2, specials[3]: 3}
-        self.itos = [specials[0], specials[1], specials[2], specials[3]]
-        self._build_vocab()
+        self.stoi = {token: idx for idx, token in enumerate(specials)}  # str to index
+        self.itos = specials[::]  # 相当于深拷贝， index to str
+        self.build_vocab()
 
-    def _build_vocab(self):
+    @process_cache(unique_key=['min_freq', 'top_k'])
+    def _build_vocab(self, file_path=None):
+        """
+        根据语料构建词表，并会将结果缓存至file_path所在的目录
+        :param file_path: 语料路径
+        :return:
+        """
         counter = Counter()
-        with open(self.file_path, encoding='utf8') as f:
+        with open(file_path, encoding='utf8') as f:
             for string_ in f:  # 遍历原始文本中的每一行
                 string_ = string_.strip()
                 counter.update(self.tokenizer(string_))  # 统计每个token出现的频率
@@ -82,44 +129,86 @@ class Vocab(object):
             self.stoi[word[0]] = i + len(self.specials)  # len(self.specials)表示已有的特殊字符
             self.itos.append(word[0])
         logging.info(f" ## 词表构建完毕，前100个词为: {list(self.stoi.items())[:100]}")
+        return self
+
+    def build_vocab(self):
+        vocab = self._build_vocab(file_path=self.file_path)
+        if self is not vocab:
+            # 当前情况为载入本地缓存的词表类对象
+            for k, v in self.__dict__.items():
+                # 将之前本地缓存的词表类对象的属性复制到当前对象
+                self.__dict__[k] = deepcopy(vocab.__dict__[k])
+        del vocab
 
     def __getitem__(self, token):
         return self.stoi.get(token, self.stoi.get(self.specials[1]))
 
     def __len__(self):
+        """
+        得到词表长度
+        :return:
+        """
         return len(self.itos)
 
 
 class LoadEnglishGermanDataset():
-    def __init__(self, train_file_paths=None, batch_size=2, min_freq=2, top_k=None):
-        # 根据训练预料建立英语和德语各自的字典
+    """
+    构建德语（src）-英语（tgt）平行语料
+    """
+    DATA_DIR = os.path.join(DATA_HOME, 'GermanEnglish')
+    DATA_FILE_PATH = {'train': {'src': os.path.join(DATA_DIR, 'train_.de'),
+                                'tgt': os.path.join(DATA_DIR, 'train_.en')},
+                      'dev': {'src': os.path.join(DATA_DIR, 'val_.de'),
+                              'tgt': os.path.join(DATA_DIR, 'val_.en')}}
+    # 指定数据集对应的路径
+    CACHE_FILE_PATH = {'train': os.path.join(DATA_DIR, 'train'),
+                       'dev': os.path.join(DATA_DIR, 'dev'),
+                       'test': os.path.join(DATA_DIR, 'test')}
 
-        self.specials = ['<PAD>', '<UNK>', '<BOS>', '<EOS>']
-        self.tokenizer = my_tokenizer()
-        self.de_vocab = Vocab(self.tokenizer['src'], file_path=train_file_paths['src'],
-                              min_freq=min_freq, top_k=top_k, specials=self.specials)
-        self.en_vocab = Vocab(self.tokenizer['tgt'], file_path=train_file_paths['tgt'],
-                              min_freq=min_freq, top_k=top_k, specials=self.specials)
-        self.PAD_IDX = self.de_vocab['<PAD>']
-        self.BOS_IDX = self.de_vocab['<BOS>']
-        self.EOS_IDX = self.de_vocab['<EOS>']
+    # 指定数据集构建完毕后的缓存路径
+
+    def __init__(self, batch_size=2, min_freq=2, top_k=None, src_inverse=True, batch_first=True):
         self.batch_size = batch_size
+        self.min_freq = min_freq
+        self.top_k = top_k
+        self.src_inverse = src_inverse
+        self.batch_first = batch_first
+        self.tokenizer = my_tokenizer()
+        logging.info(f" ## 构建源输入词表......")
+        self.src_vocab = Vocab(self.tokenizer['src'], file_path=self.DATA_FILE_PATH['train']['src'],
+                               min_freq=min_freq, top_k=top_k, specials=['<PAD>', '<UNK>'])
+        logging.info(f" ## 构建目标输入词表......")
+        self.tgt_vocab = Vocab(self.tokenizer['tgt'], file_path=self.DATA_FILE_PATH['train']['tgt'],
+                               min_freq=min_freq, top_k=top_k,
+                               specials=['<PAD>', '<UNK>', '<BOS>', '<EOS>'])
+        self.TGT_PAD_IDX = self.tgt_vocab['<PAD>']
+        self.TGT_BOS_IDX = self.tgt_vocab['<BOS>']
+        self.TGT_EOS_IDX = self.tgt_vocab['<EOS>']
 
-    def data_process(self, file_paths):
+    @process_cache(unique_key=['min_freq', 'top_k', 'batch_first'])
+    def data_process(self, file_path=None):
         """
-        将每一句话中的每一个词根据字典转换成索引的形式
-        :param file_paths:
+        将训练集或验证集或测试集中的平行语料文本转换成token
+        此处的file_path实际上指定的是构建完毕后的缓存路径
+        :param file_path:
         :return:
         """
-        raw_src_iter = iter(open(file_paths['src'], encoding="utf8"))
-        raw_tgt_iter = iter(open(file_paths['tgt'], encoding="utf8"))
+        data_name = file_path.split(os.sep)[-1]
+        # 指定源输入和目标输入各自的路径
+        raw_src_iter = iter(open(self.DATA_FILE_PATH[data_name]['src'], encoding="utf8"))
+        raw_tgt_iter = iter(open(self.DATA_FILE_PATH[data_name]['tgt'], encoding="utf8"))
         data = []
-        logging.info(f"### 正在将数据集 {file_paths} 转换成 Token ID ")
+        logging.info(f"### 正在将数据集 {file_path} 转换成 Token ID ")
         for (raw_src, raw_tgt) in tqdm(zip(raw_src_iter, raw_tgt_iter), ncols=80):
-            src_tensor_ = torch.tensor([self.de_vocab[token] for token in
-                                       self.tokenizer['src'](raw_src.rstrip("\n"))], dtype=torch.long)
-            tgt_tensor_ = torch.tensor([self.en_vocab[token] for token in
-                                       self.tokenizer['tgt'](raw_tgt.rstrip("\n"))], dtype=torch.long)
+            src_tokens = self.tokenizer['src'](raw_src.rstrip("\n"))
+            logging.debug(f"src_tokens: {src_tokens}")
+            src_tensor_ = torch.tensor([self.src_vocab[token] for token in src_tokens], dtype=torch.long)
+            logging.debug(f"src_token_ids: {src_tensor_}")
+
+            tgt_tokens = self.tokenizer['tgt'](raw_tgt.rstrip("\n"))
+            logging.info(f"tgt_tokens: {tgt_tokens}")
+            tgt_tensor_ = torch.tensor([self.tgt_vocab[token] for token in tgt_tokens], dtype=torch.long)
+            logging.info(f"tgt_token_ids: {tgt_tensor_}")
             data.append((src_tensor_, tgt_tensor_))
         # [ (tensor([ 9, 37, 46,  5, 42, 36, 11, 16,  7, 33, 24, 45, 13,  4]), tensor([ 8, 45, 11, 13, 28,  6, 34, 31, 30, 16,  4])),
         #   (tensor([22,  5, 40, 25, 30,  6, 12,  4]), tensor([12, 10,  9, 22, 23,  6, 33,  5, 20, 37, 41,  4])),
@@ -129,18 +218,25 @@ class LoadEnglishGermanDataset():
 
         return data
 
-    def load_train_val_test_data(self, train_file_paths, val_file_paths, test_file_paths):
-        train_data = self.data_process(train_file_paths)
-        val_data = self.data_process(val_file_paths)
-        test_data = self.data_process(test_file_paths)
-        logging.info(train_data)
+    def load_train_val_test_data(self, is_train=False):
+        """
+        载入对应的迭代器
+        :param is_train:
+        :return:
+        """
+        if not is_train:
+            test_data = self.data_process(self.CACHE_FILE_PATH['test'])
+            test_iter = DataLoader(test_data, batch_size=self.batch_size,
+                                   shuffle=False, collate_fn=self.generate_batch)
+            return test_iter
+
+        train_data = self.data_process(file_path=self.CACHE_FILE_PATH['train'])
+        val_data = self.data_process(file_path=self.CACHE_FILE_PATH['dev'])
         train_iter = DataLoader(train_data, batch_size=self.batch_size,
-                                shuffle=True, collate_fn=self.generate_batch)
+                                shuffle=False, collate_fn=self.generate_batch)
         valid_iter = DataLoader(val_data, batch_size=self.batch_size,
                                 shuffle=True, collate_fn=self.generate_batch)
-        test_iter = DataLoader(test_data, batch_size=self.batch_size,
-                               shuffle=True, collate_fn=self.generate_batch)
-        return train_iter, valid_iter, test_iter
+        return train_iter, valid_iter
 
     def generate_batch(self, data_batch):
         """
@@ -151,13 +247,16 @@ class LoadEnglishGermanDataset():
         :param data_batch:
         :return:
         """
-        de_batch, en_batch = [], []
-        for (de_item, en_item) in data_batch:  # 开始对一个batch中的每一个样本进行处理。
-            de_batch.append(de_item)  # 编码器输入序列不需要加起止符
+        src_batch, tgt_batch = [], []
+        for (src_item, tgt_item) in data_batch:  # 开始对一个batch中的每一个样本进行处理。
+            if self.src_inverse:  # 逆序输出
+                src_item = torch.flip(src_item, dims=[0])
+            src_batch.append(src_item)  # 编码器输入序列不需要加起止符
             # 在每个idx序列的首位加上 起始token 和 结束 token
-            en = torch.cat([torch.tensor([self.BOS_IDX]), en_item, torch.tensor([self.EOS_IDX])], dim=0)
-            en_batch.append(en)
+            tgt_item = torch.cat([torch.tensor([self.TGT_BOS_IDX]), tgt_item,
+                                  torch.tensor([self.TGT_EOS_IDX])], dim=0)
+            tgt_batch.append(tgt_item)
         # 以最长的序列为标准进行填充
-        de_batch = pad_sequence(de_batch, padding_value=self.PAD_IDX)  # [de_len,batch_size]
-        en_batch = pad_sequence(en_batch, padding_value=self.PAD_IDX)  # [en_len,batch_size]
-        return de_batch, en_batch
+        src_batch = pad_sequence(src_batch, self.batch_first, padding_value=self.TGT_PAD_IDX)  # [src_len,batch_size]
+        tgt_batch = pad_sequence(tgt_batch, self.batch_first, padding_value=self.TGT_PAD_IDX)  # [tgt_len,batch_size]
+        return src_batch, tgt_batch
