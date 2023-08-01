e63be7f1178bd36a6943581bda8a310e771e7e35
nulls8 <moon-hotel@hotmail.com>
2023-08-01 21:10:26 +0800

    数据集构建测试模块

--- test/test_LoadEnglishGermanDataset.py
+++ test/test_LoadEnglishGermanDataset.py
@@ -1,20 +1,15 @@
 import sys
-import os
 
 sys.path.append('../')
 
-from utils import DATA_HOME
 from utils import logger_init
 from utils import LoadEnglishGermanDataset
 import logging
 
 if __name__ == '__main__':
-    logger_init(log_file_name='log', log_level=logging.INFO, log_dir='./log')
-    path_de = os.path.join(DATA_HOME, 'GermanEnglish', 'train_.de')
-    path_en = os.path.join(DATA_HOME, 'GermanEnglish', 'train_.en')
-    path = {'src': path_de, 'tgt': path_en}
-    data_loader = LoadEnglishGermanDataset(train_file_paths=path, batch_size=2, min_freq=2, )
-    train_iter, valid_iter, test_iter = data_loader.load_train_val_test_data(path,path,path)
+    logger_init(log_file_name='log', log_level=logging.DEBUG, log_dir='./log')
+    data_loader = LoadEnglishGermanDataset(batch_size=2, min_freq=2,src_inverse=False)
+    train_iter, valid_iter = data_loader.load_train_val_test_data(is_train=True)
     for x, y in train_iter:
         logging.info(x)
         logging.info(y)
