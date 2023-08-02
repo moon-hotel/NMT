0c41eef490239102414a3b92d093a2502d6166cc
nulls8 <moon-hotel@hotmail.com>
2023-08-02 22:32:02 +0800

    添加注释

--- model/Seq2Seq.py
+++ model/Seq2Seq.py
@@ -2,8 +2,22 @@ import torch.nn as nn
 
 
 class Encoder(nn.Module):
+    """
+    解码器
+    """
+
     def __init__(self, embedding_size, hidden_size, num_layers, vocab_size,
                  cell_type='LSTM', bidirectional=False, batch_first=True):
+        """
+
+        :param embedding_size:
+        :param hidden_size:
+        :param num_layers:  RNN的层数
+        :param vocab_size:
+        :param cell_type:
+        :param bidirectional:
+        :param batch_first:
+        """
         super(Encoder, self).__init__()
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
@@ -27,14 +41,33 @@ class Encoder(nn.Module):
                             batch_first=self.batch_first, bidirectional=self.bidirectional)
 
     def forward(self, src_input=None):
+        """
+
+        :param src_input: [batch_size, src_len] 这种情况 batch_first 要为True
+        :return: output, (hn, cn)
+        """
         src_input = self.token_embedding(src_input)  # [batch_size, src_len, embedding_size]
         output, final_state = self.rnn(src_input)
-        return output, final_state
+        return output, final_state  # output shape: [batch_size, src_len, hidden_size]
 
 
 class Decoder(nn.Module):
+    """
+    解码器
+    """
+
     def __init__(self, embedding_size, hidden_size, num_layers, vocab_size,
                  cell_type='LSTM', bidirectional=False, batch_first=True):
+        """
+
+        :param embedding_size:
+        :param hidden_size:
+        :param num_layers: RNN层数
+        :param vocab_size:
+        :param cell_type:
+        :param bidirectional:
+        :param batch_first:
+        """
         super(Decoder, self).__init__()
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
@@ -44,9 +77,7 @@ class Decoder(nn.Module):
         self.bidirectional = bidirectional
         self.batch_first = batch_first
 
-        if cell_type == 'RNN':
-            rnn_cell = nn.RNN
-        elif cell_type == 'LSTM':
+        if cell_type == 'LSTM':
             rnn_cell = nn.LSTM
         elif cell_type == 'GRU':
             rnn_cell = nn.GRU
@@ -58,6 +89,12 @@ class Decoder(nn.Module):
                             batch_first=self.batch_first, bidirectional=self.bidirectional)
 
     def forward(self, tgt_input=None, encoder_state=None):
+        """
+
+        :param tgt_input: [batch_size, tgt_len] 这种情况 batch_first 要为True
+        :param encoder_state: encoder的state, 包含(hn, cn)两部分，这里就决定了encoder和decoder的hidden_size要一致
+        :return: output, (hn, cn)
+        """
         tgt_input = self.token_embedding(tgt_input)  # [batch_size, tgt_input, embedding_size]
         output, final_state = self.rnn(tgt_input, encoder_state)
         return output, final_state
@@ -73,6 +110,12 @@ class Seq2Seq(nn.Module):
                                cell_type, bidirectional, batch_first)
 
     def forward(self, src_input, tgt_input):
+        """
+
+        :param src_input: [batch_size, src_len]
+        :param tgt_input: [batch_size, tgt_len]
+        :return: [batch_size, tgt_len, hidden_size]
+        """
         encoder_output, encoder_state = self.encoder(src_input)
         decoder_output, _ = self.decoder(tgt_input, encoder_state)
         return decoder_output
