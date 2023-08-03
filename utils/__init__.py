50b4c5534381728ddb3369e39d173cb274a6e49a
nulls8 <moon-hotel@hotmail.com>
2023-08-03 20:27:03 +0800

    添加get_gpus接口

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,13 @@
-import os
 from .log_manage import logger_init
 from .data_helper import LoadEnglishGermanDataset
 from .data_helper import PROJECT_HOME
 from .data_helper import DATA_HOME
+from .tools import get_gpus
 
 __all__ = [
     'PROJECT_HOME',
     'DATA_HOME',
     'logger_init',
+    'get_gpus',
     'LoadEnglishGermanDataset'
 ]
