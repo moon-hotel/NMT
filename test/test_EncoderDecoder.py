bab693c02aa56c91d0e8545903d2ab1862056511
nulls8 <moon-hotel@hotmail.com>
2023-08-03 20:29:24 +0800

    优化代码

--- test/test_EncoderDecoder.py
+++ test/test_EncoderDecoder.py
@@ -5,7 +5,6 @@ sys.path.append('../')
 
 from model import Encoder
 from model import Decoder
-from model import Seq2Seq
 
 
 def test_Encoder():
@@ -47,27 +46,6 @@ def test_Decoder():
     print(output.shape)
 
 
-def test_Seq2Seq():
-    src_input = torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                                  [1, 2, 3, 3, 3, 4, 2, 1, 1]])
-    tgt_input = torch.LongTensor([[1, 2, 6, 7, 8, 9],
-                                  [1, 2, 4, 2, 1, 1]])
-    src_emb_size = 32
-    tgt_emb_size = 64
-    hidden_size = 128
-    num_layers = 2
-    src_v_size = 50
-    tgt_v_size = 60
-    cell_type = 'LSTM'
-    bidirectional = False
-    batch_first = True
-    seq2seq = Seq2Seq(src_emb_size, tgt_emb_size, hidden_size, num_layers, src_v_size, tgt_v_size,
-                      cell_type, bidirectional, batch_first)
-    output = seq2seq(src_input, tgt_input) # [batch_size, tgt_len, hidden_size]
-    print(output.shape)
-
-
 if __name__ == '__main__':
-    # test_Encoder()
-    # test_Decoder()
-    test_Seq2Seq()
+    test_Encoder()
+    test_Decoder()
