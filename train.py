e276dbe537bddc738326ca922f5aed98d0ff5b1f
nulls8 <moon-hotel@hotmail.com>
2023-08-04 23:07:21 +0800

    添加模型训练代码

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import logging
 import torch
 import os
+from copy import deepcopy
 from model import TranslationModel
+from model import greedy_decode
 from config import TranslationModelConfig
 from utils import LoadEnglishGermanDataset
 from torchtext.data.metrics import bleu_score
+from transformers import get_polynomial_decay_schedule_with_warmup
 
 
 def train(config=None):
@@ -26,7 +29,11 @@ def train(config=None):
     model = model.to(config.devices[0])
     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=data_loader.TGT_PAD_IDX)
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+    num_training_steps = len(train_iter) * config.epochs
+    scheduler = get_polynomial_decay_schedule_with_warmup(optimizer, config.num_warmup_steps,
+                                                          num_training_steps, lr_end=1e-7, power=3)
     logging.info("############开始训练############")
+    max_bleu = 0
     for epoch in range(config.epochs):
         for i, (src_input, tgt_input) in enumerate(train_iter):
             src_input, tgt_input = src_input.to(config.devices[0]), tgt_input.to(config.devices[0])
@@ -37,39 +44,60 @@ def train(config=None):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            scheduler.step()
             if i % 50 == 0:
+                bleu = compute_bleu(logits.argmax(dim=-1), tgt_out)
                 logging.info(f"Epochs[{epoch + 1}/{config.epochs}]--batch[{i}/{len(train_iter)}]"
                              f"--ppl: {round(torch.exp(loss).item(), 4)}--loss: {round(loss.item(), 4)}")
-                logging.info(f"bleu: {round(compute_bleu(logits, tgt_out), 4)}")
-                # logging.info(f"{bleu_score()}")
-                # writer.add_scalar('Training/Accuracy', acc, scheduler.last_epoch)
-            # writer.add_scalar('Training/Loss', loss.item(), scheduler.last_epoch)
-        # test_acc = evaluate(val_iter, model, config.device)
-        # test_acc = evaluate(train_iter, model, config.device)
-        # logging.info(f"Epochs[{epoch + 1}/{config.epochs}]--Acc on val {test_acc}")
-        # writer.add_scalar('Testing/Accuracy', test_acc, scheduler.last_epoch)
-        # if test_acc > max_test_acc:  # 因为
-        #     max_test_acc = test_acc
-        #     state_dict = deepcopy(model.state_dict())
-        #     torch.save(state_dict, config.model_save_path)
+                logging.info(f"bleu: {round(bleu, 4)}")
+        bleu = evaluate(config, valid_iter, model, data_loader)
+        if bleu > max_bleu:  # 因为
+            logging.info(f"bleu on valid set: {bleu}")
+            max_bleu = bleu
+            state_dict = deepcopy(model.state_dict())
+            torch.save(state_dict, config.model_save_path)
 
 
-def compute_bleu(logits, y_true):
+def evaluate(config, valid_iter, model, data_loader):
     """
-
-    :param logits: [batch_size, tgt_out_len]
-    :param y_true: [batch_size, tgt_out_len]
+    评估
+    :param config:
+    :param valid_iter:
+    :param model:
+    :param data_loader:
     :return:
     """
-    y_pred, y_true = logits.argmax(dim=-1).tolist(), y_true.tolist()
+    model.eval()
+    y_preds, y_trues = [], []
+    with torch.no_grad():
+        for src_input, tgt_input in valid_iter:
+            for src_in, tgt_in in zip(src_input, tgt_input):
+                # 此时 src_in和tgt_in的形状为 [src_len]和[tgt_len]
+                y_trues.append(tgt_in.tolist())
+                y_pred = greedy_decode(model, src_in.reshape(1, -1), data_loader.TGT_BOS_IDX,
+                                       data_loader.TGT_EOS_IDX, config.devices[0])
+                y_preds.append(y_pred)
+    model.train()
+    return compute_bleu(y_preds, y_trues)
+
+
+def compute_bleu(y_pred, y_true):
+    """
+    :param y_pred: [batch_size, tgt_out_len]  二维的list
+    :param y_true: [batch_size, tgt_out_len]  二维的list
+    :return: e.g.
+    y_pred = [[1, 2, 3, 4, 5, 6], [2, 2, 2, 3, 5, 6]]
+    y_true = [[1, 2, 3, 4, 5, 6], [2, 2, 2, 3, 5, 7]]
+    compute_bleu(y_pred, y_true) 0.88068
+    """
     y_pred = [[str(item) for item in x] for x in y_pred]
     y_true = [[[str(item) for item in x]] for x in y_true]
     return bleu_score(y_pred, y_true, max_n=4)
 
 
+
+
+
 if __name__ == '__main__':
     config = TranslationModelConfig()
     train(config)
-    # y_pred = torch.tensor([[1, 2, 3, 4, 5, 6], [2, 2, 2, 3, 5, 6]])
-    # y_true = torch.tensor([[1, 2, 3, 4, 5, 6], [2, 2, 2, 3, 5, 7]])
-
